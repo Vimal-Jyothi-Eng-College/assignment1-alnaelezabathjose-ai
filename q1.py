a829285bb5550f1ceef5262aa3192ed700eb49c9
ALNAELEZABATHJOSE <alnaelezabathjose@gmail.com>
2026-02-05 21:29:25 +0530

    commit

--- q1.py
+++ q1.py
@@ -1,9 +1,8 @@
-n = int(input("Enter N: "))
+n = int(input())
 
-print("Prime numbers from 1 to", n, "are:")
 for num in range(2, n + 1):
     is_prime = True
-    for i in range(2, num):
+    for i in range(2, int(num ** 0.5) + 1):
         if num % i == 0:
             is_prime = False
             break
